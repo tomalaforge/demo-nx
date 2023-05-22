297f0a3bfada8f85560fe47934e072a206a257d6
ThomasL <thomasl@sphinxonline.com>
2023-05-22 14:02:20 +0200

    change test

--- libs/shared/src/lib/title.pipe.spec.ts
+++ libs/shared/src/lib/title.pipe.spec.ts
@@ -3,6 +3,6 @@ import { TitlePipe } from './title.pipe';
 describe('TitlePipe', () => {
   it('create an instance', () => {
     const pipe = new TitlePipe();
-    expect(pipe.transform('toto')).toEqual('totoTitle');
+    expect(pipe.transform('toto')).toEqual('totoTitles');
   });
 });
--- libs/shared/src/lib/title.pipe.ts
+++ libs/shared/src/lib/title.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitlePipe implements PipeTransform {
   transform(value: string): string {
-    return `${value}Title`;
+    return `${value}Titles`;
   }
 }
